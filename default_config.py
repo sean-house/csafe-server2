5b565ccc3d802a0a241df77f77a8f8c6517a6c32
Sean House <33880135+sean-house@users.noreply.github.com>
2020-12-31 16:39:00 +0000

    Change ENV variable from FLASK_DB_URL to DATABASE_URL (as provided by Heroku)

--- default_config.py
+++ default_config.py
@@ -3,7 +3,7 @@ import os
 DEBUG = True
 PROPAGATE_EXCEPTIONS = True
 SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "FLASK_DB_URL", "sqlite:///dbase.db"
+        "DATABASE_URL", "sqlite:///dbase.db"
     )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_SECRET_KEY = os.environ.get('FLASK_SECRET', "qwertyuiop")
